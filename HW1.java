3e9cbda6f1edb4b5638e3a8195e0319379afb383
Alina Belaya <71881465+alina-belaya@users.noreply.github.com>
2021-09-12 18:23:56 +0300

    HW1 #2.3

---
+++ HW1.java
@@ -0,0 +1,26 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class HW1 {
+
+	public static void main(String[] args) {
+		double a, b, res;
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Input a b: ");
+		a = in.nextDouble();
+		b = in.nextDouble();
+		
+		res = Math.sqrt(a * b);
+		
+		NumberFormat formatter1 = new DecimalFormat("#0.000");
+		NumberFormat formatter2 = new DecimalFormat("#0.000E0");
+
+		System.out.println("Середнє геометричне (десяткове) = " + formatter1.format(res));
+		System.out.println("Середнє геометричне (наукове) = " + formatter2.format(res));
+
+	}
+
+}
